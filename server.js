3e5546be0cd6090d16f1730c7d2a95eea15b6656
Opal Harrison <ornrumpa.km@gmail.com>
2020-09-28 12:18:41 -0700

    updated server.js

--- server.js
+++ server.js
@@ -33,11 +33,10 @@ connection.connect(function (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-
   console.log("connected as id " + connection.threadId);
 });
 
-// ROUTING & RENDER**********************************************
+// ROUTING & RENDER***************************************************
 app.get("/", function (req, res) {
   connection.query("SELECT * FROM items;", function (err, data) {
     //handle errors
@@ -48,6 +47,68 @@ app.get("/", function (req, res) {
     res.render("index", { items: data });
   });
 });
+
+//DELETE any item on the existing grocery lust (by id)***************************************************
+app.delete("/api/items/:id", function(req, res) {
+    connection.query("DELETE FROM items WHERE id = ?", [req.params.id], function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+      //if the id not existed
+      else if (data.affectedRows === 0) {
+        return res.status(404).end();
+      }
+      //success
+      res.status(200).end();
+    });
+  });
+
+
+
+//POST/ CREATE new items ***************************************************
+app.post("/api/items", function (req, res) {
+  connection.query(
+    "INSERT INTO items(items) VALUES (?)",
+    [req.body.items],
+    function (err, data) {
+      //handle errors
+      if (err) {
+        return res.status(500).end();
+      }
+      //otherwise send id of the new item
+      res.json({ id: data.insertId });
+      console.log({ id: data.insertId });
+    }
+  );
+});
+
+//RETREIVE the new grocery list ***************************************************
+app.get("/api/items", function (req, res) {
+    connection.query("SELECT * FROM items;", function (err, data) {
+      //handle errors
+      if (err) {
+        return res.status(500).end();
+      }
+      //otherwise show the existing grocery list
+      res.json(data);
+    });
+  });
+
+
+//UPDATE request ***************************************************
+
+app.put("/api/items/:id", function(req, res) {
+    connection.query("UPDATE items SET items = ? WHERE id = ?", [req.body.items, req.params.id], function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+      else if (data.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
+  
 
 // Start server,listento client requests.
 app.listen(PORT, function () {
